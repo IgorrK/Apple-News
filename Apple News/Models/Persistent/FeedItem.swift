30d29d1143a284ae936f38093fa2e00459e47244
igor-iOS <igor.kulik@techs.com.ua>
2016-08-08 14:19:11 +0300

    Improved architecture
    Applied Single Responsibility principle for the classes responsible for
    persistent storage operations.

--- Apple News/Models/Persistent/FeedItem.swift
+++ Apple News/Models/Persistent/FeedItem.swift
@@ -15,62 +15,24 @@ class FeedItem: NSManagedObject {
     
     private static let EntityName = "FeedItem"
     
+    /**
+     *  A structure with names of managed object's attributes.
+     */
+    struct AttributeKeys {
+        static let Title = "title"
+        static let Body = "body"
+        static let EncodedContent = "encodedContent"
+        static let pubDate = "pubDate"
+    }
+    
     // - Class methods
     
     /**
-     Get Core Data entity name
+     Returns the name of entity used for storing in CoreData.
      
-     - returns: Entity name
+     - returns: The name of the enity.
      */
     static func entityName() -> String {
         return EntityName
-    }
-    
-    /**
-     Create new entity object and insert it
-     into the specified context
-     
-     - parameter context: Managed Object Context to insert a new object
-
-     - returns: Created object
-     */
-    static func insertNewObjectIntoContext(context: NSManagedObjectContext) -> FeedItem? {
-        return NSEntityDescription.insertNewObjectForEntityForName(EntityName, inManagedObjectContext: context) as? FeedItem
-    }
-    
-    /**
-     Create new entity object with data from dictionary
-     and insert it into the specified context
-
-     
-     - parameter dictionary: Dictionary containing values for object properties
-     - parameter inContext:  Managed Object Context to insert a new object
-     
-     - returns: Created object
-     */
-    static func initNewObject(dictionary: FeedItemData, inContext: NSManagedObjectContext) -> NSManagedObject? {
-        // Create object
-        guard let feedItem = FeedItem.insertNewObjectIntoContext(inContext) else {
-            print("error creating object")
-            return nil
-        }
-        
-        // Title
-        feedItem.title = dictionary[XMLParser.keys.title]
-        
-        // Plain and HTML-encoded body. Decided to store both because
-        // HTML-encoded text is required for displaying hyperlinks on Feed Details screen
-        // and plain text is used in Feed table, and transforming from HTML each
-        // time would overload the main thread, causing animation lags and 
-        // affecting preformance.
-        feedItem.body = dictionary[XMLParser.keys.description]
-        feedItem.encodedContent = dictionary[XMLParser.keys.encodedContent]
-        
-        // Date
-        let dateStr = dictionary[XMLParser.keys.pubDate]!
-        DateFormattingUtility.sharedInstance.configureForUsage(UsageCase.PubDate)
-        feedItem.pubDate = DateFormattingUtility.sharedInstance.dateFromString(dateStr)
-        
-        return feedItem
     }
 }
--- Apple News/Utilities/CoreDataManager.swift
+++ Apple News/Utilities/CoreDataManager.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-/// Separate class responsible for interaction with CoreData
+/// Separate class responsible for interaction with CoreData.
 final class CoreDataManager: NSObject {
     
     // MARK: - Properties
@@ -21,18 +21,18 @@ final class CoreDataManager: NSObject {
     // MARK: - Public methods
     
     /**
-     Transform an array of data dictionaries
-     into FeedItem objects and save them to the storage
+     Transforms an array of data dictionaries
+     into FeedItem objects and save them to the storage.
      
-     - parameter data: Array containing FeedItems' data to save
+     - parameter data: Array containing FeedItems' data to save.
      */
     func storeFeedItemsFromData(data: [FeedItemData])  {
         // Delete the old feed
         deleteOldFeedItems()
 
         // Transform data to FeedItem objects
-        data.forEach { feedItemData in
-            FeedItem.initNewObject(feedItemData, inContext: managedObjectContext)
+        data.forEach {
+            createNewFeedItemFromDictionary($0)
         }
         
         // Save changes
@@ -40,17 +40,18 @@ final class CoreDataManager: NSObject {
     }
     
     /**
-     Fetch current FeedItems from the database,
-     sorted descending by date
+     Fetches current FeedItems from the database,
+     sorted descending by date.
      
-     - returns: Array of FeedItems
+     - returns: Array of FeedItem objects.
      */
     func getCurrentFeedItems() -> [FeedItem] {
         // Form a fetch request for items, sorted by date
         let fetchRequest = NSFetchRequest(entityName: FeedItem.entityName())
-        let sortDescriptor = NSSortDescriptor(key: "pubDate", ascending: false)
-        
+        let sortDescriptor = NSSortDescriptor(key: FeedItem.AttributeKeys.pubDate, ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Execute fetch request
         do {
             let results = try managedObjectContext.executeFetchRequest(fetchRequest)
             return results as! [FeedItem]
@@ -63,7 +64,55 @@ final class CoreDataManager: NSObject {
     // MARK: - Private methods
     
     /**
-     Delete old FeedItems from the database
+     Creates an instance of the class for the entity with a given name,
+     inserted into the default managed object context.
+     
+     - parameter entityName: The name of an entity.
+     
+     - returns: A new instance of the class for the entity with a given name.
+     */
+    private func insertNewObjectForEntityForName(entityName: String) -> NSManagedObject {
+        return NSEntityDescription.insertNewObjectForEntityForName(entityName, inManagedObjectContext: managedObjectContext)
+    }
+    
+    
+    /**
+     Creates and an instance of FeedItem class, inserted into the default 
+     managed object context and configures it with the parameters given in dictionary.
+     
+     - parameter dictionary: Dictionary containing paramaters of instance configuration.
+     
+     - returns: A new configured instance of FeedItem class
+     */
+    private func createNewFeedItemFromDictionary(dictionary: FeedItemData) -> FeedItem? {
+        // Create basic instance
+        guard let feedItem = insertNewObjectForEntityForName(FeedItem.entityName()) as? FeedItem else {
+            print("error creating object")
+            return nil
+        }
+        
+        // Set attributes:
+        // Title
+        feedItem.title = dictionary[XMLParser.keys.title]
+        
+        // Plain and HTML-encoded body. Decided to store both because
+        // HTML-encoded text is required for displaying hyperlinks on Feed Details screen
+        // and plain text is used in Feed table, and transforming from HTML each
+        // time would overload the main thread, causing animation lags and
+        // affecting preformance.
+        feedItem.body = dictionary[XMLParser.keys.description]
+        feedItem.encodedContent = dictionary[XMLParser.keys.encodedContent]
+        
+        // Date
+        let dateStr = dictionary[XMLParser.keys.pubDate]!
+        DateFormattingUtility.sharedInstance.configureForUsage(UsageCase.PubDate)
+        feedItem.pubDate = DateFormattingUtility.sharedInstance.dateFromString(dateStr)
+        
+        return feedItem
+    }
+    
+    /**
+     Deletes previously stored feed items from the database.
      */
     private func deleteOldFeedItems() {
         let fetchRequest = NSFetchRequest(entityName: FeedItem.entityName())
@@ -76,7 +125,7 @@ final class CoreDataManager: NSObject {
     }
     
     /**
-     Generic method for saving latest changes to the MOC
+     Saves latest changes to the default managed object context.
      */
     private func updateMOC() {
         do {
@@ -85,4 +134,5 @@ final class CoreDataManager: NSObject {
             print("!!MOC error: \(error)")
         }
     }
+    
 }
